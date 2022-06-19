2f769caf43f178025f3cb072b19f365a967bd3c4
Wasawat Pruthipunyaskul <wasawatp@NBAAP2100004.local>
2022-06-19 11:23:12 +0700

    feat: add directory, category-item components

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,19 @@
+import './category-item.style.scss'
+
+const CategoryItem = (props)=>{
+    const {title, imageUrl} = props.category;
+    return(
+    <div
+        className="category-container"
+        style= {{ backgroundImage: `url("${imageUrl}")`}}
+    >
+        <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+        </div>
+    </div>
+    )
+
+}
+
+export default CategoryItem
