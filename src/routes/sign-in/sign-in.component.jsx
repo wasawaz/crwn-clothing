b17226e90c476e9d059acb8c057cf40cd21e5e38
Wasawat Pruthipunyaskul <wasawatp@NBAAP2100004.local>
2022-06-26 20:51:19 +0700

    feat: add sign-in with redirect.

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,39 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+import {
+  auth,
+  createUserDocumentFromAuth,
+  signInWithGooglePopup,
+  signInWithGoogleRedirect,
+} from "../../utils/firebase/firebase.utils";
 
-import { createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { getRedirectResult } from "firebase/auth";
+
+import { useEffect } from "react";
 
 const SignIn = () => {
+
+    useEffect(() => {
+        async function fetchData() {
+          // You can await here
+          const response = await getRedirectResult(auth);
+          if (response) {
+            const userDocRef = await createUserDocumentFromAuth(response.user);
+          }
+        }
+        fetchData();
+      }, []); // Or [] if effect doesn't need props or state
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>
     </div>
   );
 };
