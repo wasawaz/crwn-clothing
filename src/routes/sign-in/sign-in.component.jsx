fed66a64fa6185501c03ced1d414467a2208c0dc
Wasawat Pruthipunyaskul <wasawatp@NBAAP2100004.local>
2022-07-10 16:11:01 +0700

    chore: remove signInWithGoogleRedirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,26 +1,10 @@
 import {
-  auth,
   createUserDocumentFromAuth,
   signInWithGooglePopup,
-  signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
 
-import { getRedirectResult } from "firebase/auth";
-
-import { useEffect } from "react";
 
 const SignIn = () => {
-
-    useEffect(() => {
-        async function fetchData() {
-          // You can await here
-          const response = await getRedirectResult(auth);
-          if (response) {
-            const userDocRef = await createUserDocumentFromAuth(response.user);
-          }
-        }
-        fetchData();
-      }, []); // Or [] if effect doesn't need props or state
 
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
@@ -31,9 +15,6 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign in with Google Redirect
-      </button>
     </div>
   );
 };
